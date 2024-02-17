5bf0acbe387a4f3f27e82e38473097b039275df6
fady <32818953+FadyDMK@users.noreply.github.com>
2024-02-17 19:19:02 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const myLibrary = [];
+class Book{
+    constructor(name,author,pages){
+        this.name=name;
+        this.author=author;
+        this.pages=pages;
+    }
+
+    deleteFrom(library){
+        library.forEach(book => {
+
+            
+        });
+    }
+    
+}
