b3c243655040b315d2510897f40686e21eacf8cf
fady <32818953+FadyDMK@users.noreply.github.com>
2024-02-17 21:10:28 +0100

    added display function for books/Read button works

--- js/main.js
+++ js/main.js
@@ -1,16 +1,83 @@
+const books = document.querySelector('.main');
+
+
+
 const myLibrary = [];
-class Book{
-    constructor(name,author,pages){
-        this.name=name;
-        this.author=author;
-        this.pages=pages;
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
     }
 
-    deleteFrom(library){
-        library.forEach(book => {
+    display() {
+        const book = document.createElement('div');
+        book.classList.add('book');
+        book.setAttribute('data-read', this.read);
 
-            
-        });
+        const text = document.createElement('div');
+        text.classList.add('text');
+
+
+        const titleDisplay = document.createElement('div')
+        titleDisplay.classList.add('title');
+        titleDisplay.textContent = `Title: ${this.title}`;
+
+        const authorDisplay = document.createElement('div')
+        authorDisplay.classList.add('author');
+        authorDisplay.textContent = `Author: ${this.author}`;
+
+        const pagesDisplay = document.createElement('div')
+        pagesDisplay.classList.add('pages');
+        pagesDisplay.textContent = `Number of Pages: ${this.pages}`;
+
+
+
+        const buttons = document.createElement('div');
+        buttons.classList.add('buttons');
+
+        const readBtn = document.createElement('button');
+        readBtn.textContent = 'Read';
+        readBtn.classList.add('readbtn');
+        readBtn.addEventListener('click', () => {
+            this.read=!this.read;
+            book.setAttribute('data-read', this.read);
+
+        })
+
+
+
+
+
+        const removeBtn = document.createElement('button');
+        removeBtn.textContent = 'Delete';
+
+        text.appendChild(titleDisplay);
+        text.appendChild(authorDisplay);
+        text.appendChild(pagesDisplay);
+        book.appendChild(text);
+        book.appendChild(buttons);
+
+        buttons.appendChild(readBtn);
+        buttons.appendChild(removeBtn);
+
+
+
+
+        books.appendChild(book);
+
+
+
     }
-    
+
 }
+
+let test = new Book('berserk', 'Miura', 130, true);
+
+test.display();
+test.display();
+test.display();
+
+
+
